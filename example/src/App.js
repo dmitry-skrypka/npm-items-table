6e04d23f59c39049418200b67db714b27ce97c31
dmitry-skrypka <dimitry.skripka@gmail.com>
2018-10-19 05:04:46 +0300

    Merge branch 'master' of https://github.com/dmitry-skrypka/npm-items-table
    # Conflicts:
    #	README.md

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,58 @@
 import React, { Component } from 'react'
+import styled from 'styled-components';
+
 
 import ExampleComponent from 'npm-pb-items-table'
+export const Data = [
+  {
+    id: 1,
+    category: 'Sporting Goods',
+    price: '49.99',
+    qty: '12',
+    name: 'football',
+  },
+  {
+    id: 2,
+    category: 'Sporting Goods',
+    price: '9.99',
+    qty: '15',
+    name: 'baseball',
+  },
+  {
+    id: 3,
+    category: 'Sporting Goods',
+    price: '29.99',
+    qty: '14',
+    name: 'basketball'
 
+  },
+  {
+    id: 4,
+    category: 'Electronics',
+    price: '99.99',
+    qty: '34',
+    name: 'iPod Touch',
+  },
+  {
+    id: 5,
+    category: 'Electronics',
+    price: '399.99',
+    qty: '12',
+    name: 'iPhone 5',
+  },
+  {
+    id: 6,
+    category: 'Electronics',
+    price: '199.99',
+    qty: '23',
+    name: 'nexus 7',
+  },
+];
 export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <ExampleComponent data={Data} BgColor={"#D8E6F3"} />
       </div>
     )
   }
